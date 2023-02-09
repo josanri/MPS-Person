75e378c9198d8d97e91433bdb36bb995be6feb0e
José Manuel <jmsanchezrico@gmail.com>
2023-02-09 14:19:04 +0100

    Person documentation

--- src/main/java/org/mps/Person.java
+++ src/main/java/org/mps/Person.java
@@ -12,6 +12,12 @@ public class Person {
     private final int age;
     private final Gender gender;
 
+    /**
+     * Constructor of person
+     * @param name name of the person, not nullable.
+     * @param age age of the person, not negative.
+     * @param gender, gender, female or male.
+     */
     public Person(String name, int age, Gender gender) {
         if (name == null)
         {
@@ -26,6 +32,13 @@ public class Person {
         this.gender = gender;
     }
 
+    /**
+     * Calculates the average of ages of male and female people
+     * and returns the result in the position of the array
+     * @param list list of person, not nullable or empty
+     * @return a double array where double[0] means the average
+     * age of male and double[1] the average age of females
+     */
     public static double[] averageAgePerGender(List<Person> list) {
         double[] averageAgePerGender;
 
