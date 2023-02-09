1cc2f596960a6b30196863e6b85781cedbfb29f3
José Manuel <jmsanchezrico@gmail.com>
2023-02-09 14:14:38 +0100

    Calculated average per gender

--- src/main/java/org/mps/Person.java
+++ src/main/java/org/mps/Person.java
@@ -13,13 +13,60 @@ public class Person {
     private final Gender gender;
 
     public Person(String name, int age, Gender gender) {
+        if (name == null)
+        {
+            throw new IllegalArgumentException("Null name");
+        }
+        if (age < 0)
+        {
+            throw new IllegalArgumentException("Negative age");
+        }
         this.name = name;
         this.age = age;
         this.gender = gender;
     }
 
     public static double[] averageAgePerGender(List<Person> list) {
-        return null;
+        double[] averageAgePerGender;
+
+        checkList(list);
+        averageAgePerGender = new double[2];
+        calculateAverage(list, Gender.male, averageAgePerGender, 0);
+        calculateAverage(list, Gender.female, averageAgePerGender, 1);
+        return averageAgePerGender;
+    }
+
+    private static void checkList(List<Person> list) {
+        if (list == null)
+        {
+            throw new IllegalArgumentException("Null list");
+        }
+        if (list.size() == 0)
+        {
+            throw new IllegalArgumentException("Empty list given");
+        }
+    }
+
+    private static void calculateAverage(List<Person> list, Gender gender, double[] averageAgePerGender, int x) {
+        int size = 0;
+        int sumOfAges = 0;
+
+        for (Person person : list) {
+            if (person.getGender().equals(gender))
+            {
+                sumOfAges += person.getAge();
+                size++;
+                if (sumOfAges < 0)
+                {
+                    throw new RuntimeException("Overflow when adding the total of ages");
+                }
+            }
+        }
+        if (size > 0) {
+            averageAgePerGender[x] = (double) sumOfAges / size;
+        } else {
+            averageAgePerGender[x] = 0.0;
+        }
     }
 
     public String getName() {
